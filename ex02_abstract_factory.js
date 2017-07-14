1bf2d7c25ffbe7707cec814123b0bf28366f4df0
Wagner Fonseca <wagner.fonseca@gmail.com>
2017-07-14 14:36:39 -0300

    sample abstract factory

---
+++ ex02_abstract_factory.js
@@ -0,0 +1,49 @@
+function Car(options) {
+  this.doors = options.doors || 4
+  this.state = options.state || 'Brand new'
+  this.color = options.color || 'Silver'
+}
+
+function Truck(options ) {
+  this.state = options.doors || 'used'
+  this.wheelSize = options.wheelSize || 'large'
+  this.color = options.color || 'blue'
+}
+
+
+
+var abstractVehicleFactory = (function () {
+
+  // Storage for our vehicle types
+  var types = {}
+
+  return {
+    getVehicle: function(type, customizations) {
+      var Vehicle = types[type]
+
+      return (Vehicle ? new Vehicle(customizations) : null)
+    },
+
+    registerVehicle: function(type, Vehicle) {
+      var proto = Vehicle.prototype      
+      types[type] = Vehicle
+      return abstractVehicleFactory
+    }
+  }
+
+})()
+
+// usage
+abstractVehicleFactory.registerVehicle( 'car', Car )
+abstractVehicleFactory.registerVehicle( 'truck', Truck )
+
+// Instantiate a new car based on the abstract vehicle type
+var car = abstractVehicleFactory.getVehicle(
+  'car',
+  {
+    color: 'lime green',
+    state: 'like new'
+  }
+)
+
+console.log(car)
